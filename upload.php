b57e76711fbeacb43c18f640b2f8257b9f87c586
RyuichiTaira <115512258+RyuichiTaira@users.noreply.github.com>
2022-10-12 09:26:04 +0900

    Add files via upload

--- upload.php
+++ upload.php
@@ -5,7 +5,24 @@
     //アップロード処理
     if(isset($_FILES['image']) && is_uploaded_file($_FILES['image']['tmp_name'])){
         $old_name = $_FILES['image']['tmp_name'];
-        $new_name = $_FILES['image']['name'];
+        $new_name = date("YmdHis");
+        $new_name .= mt_rand();
+        $size = getimagesize($_FILES['image']['tmp_name']);
+        var_dump($size);
+        switch($size[2]){
+            case IMAGETYPE_JPEG:
+                $new_name .= '.jpeg';
+                break;
+            case IMAGETYPE_GIF:
+                $new_name .= '.gif';
+                break;
+            case IMAGETYPE_PNG:
+                $new_name .= '.png';
+                break;
+            default:
+                header('Location: https://www.yahoo.co.jp/');
+                exit();
+        }
 
         if(move_uploaded_file($old_name, 'album/'.$new_name)){
             $msg = "アップロードしました";
@@ -32,9 +49,7 @@
         <?php 
             if($msg){
                 echo '<div class="alert alert-'.$alert.'" role = "alert">'.$msg.'</div>';
-            }
-
-        
+            }        
         ?>
 
        <form action="upload.php" method="post" enctype="multipart/form-data">
@@ -44,6 +59,9 @@
            </div>
            <input type="submit" value="アップロードする" class="btn btn-primary">
        </form>
+       <?php 
+        var_dump($_FILES);
+        ?>
     </body>
 
 </html>
